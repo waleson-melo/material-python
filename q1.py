a43935a52742970bcc19922c13f099f9fcdec77d
André <andre.lvmb@hotmail.com>
2025-05-21 18:23:10 -0300

    Arquivo Exercício

---
+++ q1.py
@@ -0,0 +1,41 @@
+"""
+idade = 25
+altura = 1.75
+nome = "Maria"
+ligado = True
+idade = "str"
+
+#print(type(idade))
+
+#print(5//2)
+# x = 1
+# y = 2
+# z = False
+
+# if x == y:
+#     print("igual")
+# elif x != y and y < x:
+#     print("diferente2")
+# elif not z:
+#     print("False")
+
+# else:
+#     print("diferente")
+
+#print("Bem-vindo ao curso do Waleshow!")
+
+# nome = input("Digite seu nome:")
+# idade = int (input("Digite sua idade:"))
+x = bool (input("Digite um nø:"))
+y = bool (input("Digite outro nø:"))
+# print ("Olá, ", nome, idade)
+
+print(x + y)
+
+"""
+
+nome = "André"
+idade = 35
+altura = 1.8
+
+print(f"{nome}, {idade}, {altura}")
